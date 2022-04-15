cadbcffa6d7689ecb44e0c320ce8aaef3fc70a43
ilambang <imanherlambang14@gmail.com>
2022-04-15 17:37:35 +0700

    Update tanpa include

--- src/shell_simulation_node.cpp
+++ src/shell_simulation_node.cpp
@@ -1,11 +1,52 @@
-#include <shell_simulation/example_publisher.h>
+/*Include ROS library and it's dependency*/
+#include <ros/ros.h>
+#include <stdlib.h>
+#include <string>
+#include <queue>
+#include <cmath>
+
+/*Include standard format library*/
+#include <std_msgs/Float64.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Bool.h>
+#include <nav_msgs/Odometry.h>
+
+/*Create object for publishing data or publisher*/
+ros::Publisher brake_Pub;
+ros::Publisher throttle_Pub;
+ros::Publisher steering_Pub;
+ros::Publisher gear_Pub;
+ros::Publisher handbrake_Pub;
+
+/*Create object for subscribing sensor data*/
+ros::Subscriber odometry_Sub;
+ros::Subscriber speedometer_Sub;
+
 
 double steering_value;
+float lat,longitude,alt;
+
+// void chatterCallback(const sensor_msgs::NavSatFix::ConstPtr& msg)
+// {
+//   ROS_INFO("I heard: [%s]", msg->data.c_str());
+//   std::cout << msg->longitude << msg->latitude << msg->altitude << std::endl;
+// }
 
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "shell_simulation");
+    //Initialize the ros
+    ros::init(argc, argv, "shell_simulation_node");
+
+    //Creating Ros handle
+    ros::NodeHandle n;
+
+    //Subsribe to certain topic
+    brake_Pub = nh.advertise<std_msgs::Float64>("brake_command", 8);
+    throttle_Pub = nh.advertise<std_msgs::Float64>("throttle_command", 8);
+    steering_Pub = nh.advertise<std_msgs::Float64>("steering_command", 8);
+    gear_Pub = nh.advertise<std_msgs::String>("gear_command", 8);
+    handbrake_Pub = nh.advertise<std_msgs::Bool>("handbrake_command", 8);
     
-    ExamplePublisher node;
 
     ros::Rate rate(10);
 
